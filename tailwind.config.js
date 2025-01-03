eb346091834d4b4ad8f3c346710d1d52634757e8
Toskyuu <robert.laski10@gmail.com>
2025-01-03 08:20:40 +0100

    zrobienie nagłówka, sidebaru i zmiany motywu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './index.html',
-        './src/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: 'class',
-    extend: {
+module.exports = {
+    content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+    darkMode: 'selector',
+    theme: {
         colors: {
             primary: {
-                light: '#ADC698',
-                dark: '#ADC698',
+                DEFAULT: '#ADC698',
             },
             secondary: {
-                light: '#9067C6',
-                dark: '#9067C6',
+                DEFAULT: '#9067C6',
             },
             background: {
                 light: '#F7F5FB',
@@ -29,35 +24,35 @@ export default {
             },
             error: '#FF6B6B',
         },
-        Family: {
-            sans: ['Inter', 'sans-serif'],
-            heading: ['Poppins', 'sans-serif'],
-            mono: ['Roboto Mono', 'monospace'],
-        },
-        fontSize: {
-            tiny: '0.75rem',
-            base: '1rem',
-            lg: '1.125rem',
-            xl: '1.25rem',
-            '2xl': '1.5rem',
-            '3xl': '1.875rem',
-        },
-        spacing: {
-            72: '18rem',
-            84: '21rem',
-            96: '24rem',
-        },
-        boxShadow: {
-            'soft': '0 2px 4px rgba(0, 0, 0, 0.1)',
-            'hard': '0 4px 6px rgba(0, 0, 0, 0.2)',
-        },
-        borderRadius: {
-            'small': '0.125rem',
-            'large': '1rem',
-        },
-
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                heading: ['Poppins', 'sans-serif'],
+                mono: ['Roboto Mono', 'monospace'],
+            },
+            fontSize: {
+                tiny: '0.75rem',
+                base: '1rem',
+                lg: '1.125rem',
+                xl: '1.25rem',
+                '2xl': '1.5rem',
+                '3xl': '1.875rem',
+            },
+            spacing: {
+                72: '18rem',
+                84: '21rem',
+                96: '24rem',
+            },
+            boxShadow: {
+                'soft': '0 2px 4px rgba(0, 0, 0, 0.1)',
+                'hard': '0 4px 6px rgba(0, 0, 0, 0.2)',
+            },
+            borderRadius: {
+                'small': '0.125rem',
+                'large': '1rem',
+            },
+        }
     },
-
     plugins: [],
 };
 
