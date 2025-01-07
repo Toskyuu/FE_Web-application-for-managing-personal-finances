8df903d40d1eb5b86e61c7603eb900f83d6625ca
Toskyuu <robert.laski10@gmail.com>
2025-01-07 11:51:04 +0100

    rejestracja działa, jest fajnie

--- src/components/Elements/Forms/RegisterForm.tsx
+++ src/components/Elements/Forms/RegisterForm.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import {useForm, SubmitHandler} from "react-hook-form";
-import {DefaultButton, MainCard} from "@/components";
-import {useAuth} from "@/hooks/useAuth.tsx";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { DefaultButton, MainCard } from "@/components";
+import { useAuth } from "@/hooks/useAuth.tsx";
 
-interface LoginFormInputs {
+interface RegisterFormInputs {
+    username: string;
     email: string;
     password: string;
 }
 
-const LoginForm: React.FC = () => {
-    const {register, handleSubmit, formState: {errors}} = useForm<LoginFormInputs>();
-    const { logIn } = useAuth();
+const RegisterForm: React.FC = () => {
+    const {  register: formRegister, handleSubmit, formState: { errors } } = useForm<RegisterFormInputs>();
+    const { register } = useAuth();
 
-    const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
-        await logIn(data.email, data.password);
+    const onSubmit: SubmitHandler<RegisterFormInputs> = async (data) => {
+        await register(data.username, data.email, data.password);
     };
 
     return (
@@ -25,16 +26,29 @@ const LoginForm: React.FC = () => {
                 width="w-full max-w-md"
             >
                 <form onSubmit={handleSubmit(onSubmit)} className="w-full">
-                    <h2 className="text-3xl font-semibold text-center mb-6">Logowanie</h2>
+                    <h2 className="text-3xl font-semibold text-center mb-6">Rejestracja</h2>
+
+                    <div className="mb-4">
+                        <label htmlFor="username" className="block text-md font-medium">
+                            Nazwa użytkownika
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            {...formRegister("username", { required: "Nazwa użytkownika nie może być pusta" })}
+                            className={`mt-1 p-2 block w-full border rounded-md text-text-light ${errors.username ? "border-red-500" : "border-gray-300"}`}
+                        />
+                        {errors.username && <span className="text-md text-red-500">{errors.username.message}</span>}
+                    </div>
 
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-md font-medium">
-                            E-mail
+                            E-mail (Login)
                         </label>
                         <input
                             type="email"
                             id="email"
-                            {...register("email", {required: "E-mail nie może być pusty"})}
+                            {...formRegister("email", { required: "E-mail nie może być pusty" })}
                             className={`mt-1 p-2 block w-full border rounded-md text-text-light ${errors.email ? "border-red-500" : "border-gray-300"}`}
                         />
                         {errors.email && <span className="text-md text-red-500">{errors.email.message}</span>}
@@ -47,14 +61,14 @@ const LoginForm: React.FC = () => {
                         <input
                             type="password"
                             id="password"
-                            {...register("password", {required: "Hasło nie może być puste"})}
+                            {...formRegister("password", { required: "Hasło nie może być puste" })}
                             className={`mt-1 p-2 block w-full border rounded-md text-text-light ${errors.password ? "border-red-500" : "border-gray-300"}`}
                         />
                         {errors.password && <span className="text-md text-red-500">{errors.password.message}</span>}
                     </div>
 
                     <DefaultButton
-                        text= "Zaloguj się"
+                        text="Zarejestruj się"
                         onClick={handleSubmit(onSubmit)}
                         bgColor="bg-success"
                         color="text-text-dark"
@@ -63,11 +77,10 @@ const LoginForm: React.FC = () => {
                         fontSize="text-2xl"
                         minwidth="w-full"
                     />
-
                 </form>
             </MainCard>
         </div>
     );
 };
 
-export default LoginForm;
+export default RegisterForm;
--- src/pages/Anon/RegisterPage.tsx
+++ src/pages/Anon/RegisterPage.tsx
@@ -1,8 +1,20 @@
-import LoginForm from "@/components/Elements/Forms/LoginForm.tsx";
-import React from 'react';
+import RegisterForm from "@/components/Elements/Forms/RegisterForm.tsx";
+import React, {useEffect} from 'react';
+import {useAuth} from "@/hooks/useAuth.tsx";
+import {useNavigate} from "react-router-dom";
 
 const RegisterPage: React.FC = () => {
+    const {isAuthenticated} = useAuth();
+    const navigate = useNavigate();
+
+    // useEffect(() => {
+    //     if (isAuthenticated) {
+    //         navigate('/');
+    //     }
+    // }, [isAuthenticated, navigate]);
+
     return (
+
         <div>
             <RegisterForm/>
         </div>
